c57595985344f8665af53f6046cd5a725fd3fccb
supperchong <supperchong@gmail.com>
2021-02-25 20:23:25 +0800

    chore: clean code

--- src/graph.ts
+++ src/graph.ts
@@ -1,7 +1,7 @@
 /**
- * 根据无向图邻接数组求连通度
- * @param {number[][]} adjArr 邻接数组
- * @param {number} n 顶点个数
+ * Calculate the connectivity degree according to the undirected graph adjacency array.
+ * @param {number[][]} adjArr adjacency array
+ * @param {number} n vertex number
  * @category Graph
  * @mermaid
  * graph LR
@@ -38,7 +38,7 @@ function dfs(node: number, set: Set<number>, adjArr: number[][]) {
 }
 
 /**
- * 根据无向图邻接矩阵求连通度
+ * Calculate the connectivity degree according to the undirected graph adjacency matrix.
  * @category Graph
  * @param {number[][]} adjMatrix 邻接矩阵
  */
@@ -63,72 +63,3 @@ function dfs2(index: number, set: Set<number>, isConnected: number[][]) {
     }
   }
 }
-
-interface NodeMap {
-  [key: number]: number
-}
-interface Mark {
-  [key: number]: boolean
-}
-// 匈牙利二分匹配
-/**
- * 	hungarian
- * 	bipartite graph contains two vertex set X and Y
- * 	@category Graph
- *	@param m the number of X
- */
-export function hungarian(m: number, adj: number[][]) {
-  //
-  let map: NodeMap = {}
-  for (let i = 0; i < m; i++) {
-    if (!Number.isInteger(map[i])) {
-      let markd: Mark = {}
-      dfs3(i, markd, adj, map)
-    }
-  }
-  return map
-}
-
-function dfs3(
-  node: number,
-  markd: Mark,
-  adj: number[][],
-  map: NodeMap
-): boolean {
-  markd[node] = true
-  for (const adjNode of adj[node]) {
-    if (markd[adjNode]) {
-      continue
-    }
-    markd[adjNode] = true
-    if (
-      !Number.isInteger(map[adjNode]) ||
-      dfs3(map[adjNode], markd, adj, map)
-    ) {
-      map[node] = adjNode
-      map[adjNode] = node
-      return true
-    }
-  }
-  return false
-}
-
-// /**
-//  * bipartite matching algorithms
-//  *
-//  * the [Hopcroft–Karp algorithm](https://en.wikipedia.org/wiki/Hopcroft%E2%80%93Karp_algorithm) is an algorithm that takes as input a bipartite graph and produces as output a maximum cardinality matching
-//  *
-//  * bipartite graph contains two vertex set X and Y
-//  * @param m the number of X
-//  * @param adj the adjacency array
-//  */
-// export function hopcroftKarp(m: number, adjArr: number[][]) {
-
-// }
-// function dfs4() {
-
-// }
-
-// function bfs4() {
-
-// }
